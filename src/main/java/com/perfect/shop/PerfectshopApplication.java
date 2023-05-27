1a07d05fc3aac3511b274508d80dec386897f635
Khan <Khan@DESKTOP-QOPBBQ3.mshome.net>
2023-05-27 23:49:52 +0530

    Initial commit

---
+++ src/main/java/com/perfect/shop/PerfectshopApplication.java
@@ -0,0 +1,13 @@
+package com.perfect.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfectshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfect/shop/PerfectshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfect.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
